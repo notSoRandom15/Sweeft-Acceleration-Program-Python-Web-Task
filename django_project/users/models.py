b306c003ac63bc1ff4d3f38e6d9d1fab890b17f8
demetre <deme.barbakadze2@gmail.com>
2024-03-09 15:11:20 +0400

    modified abstracuser

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from .managers import UserManager
+
 
 class User(AbstractUser):
     name = models.CharField(max_length=255)
-    email = models.CharField(max_length=255,unique=True)
+    email = models.EmailField(max_length=255,unique=True)
     password = models.CharField(max_length=255)
 
-    username = None
+
+    # username = None
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+    username = models.CharField(max_length=30, unique=True, null=True, blank=True)
+
+
+    objects = UserManager()
+
